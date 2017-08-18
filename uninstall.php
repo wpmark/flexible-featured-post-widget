4240905f188bc82a0154c6cd9bb81e2298ca2fb9
Karen Attfield <karenlattfield@gmail.com>
2017-08-18 16:25:41 +0100

    Modified uninstall.php to update text-domain and remove global  reference

--- uninstall.php
+++ uninstall.php
@@ -5,7 +5,7 @@
  
 /* If uninstall not called from WordPress, then exit. */
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	wp_die( sprintf( __( '%s should only be called when uninstalling the plugin.', 'wdp-plugin' ), '<code>' . __FILE__ . '</code>' ) );
+	wp_die( sprintf( __( '%s should only be called when uninstalling the plugin.', 'flexible-featured-post-widget' ), '<code>' . __FILE__ . '</code>' ) );
 }
 
 
@@ -16,7 +16,6 @@ delete_option('widget_FFPW_Flexible_Featured_Post');
 /* Deleting post meta data added by plugin */
 function delete_ffpw_meta() {
    
-    global $wpdb;
     $posts = get_posts( array(
         'numberposts' => -1,
         'post_type' => 'post',
